a8342579838d62bf39f055970f597df07fd02971
ricardo <ricardo.moreno@osgroup.co>
2021-07-21 18:23:24 -0500

    ADD password encrypt

--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -1,8 +1,12 @@
 import * as repositories from "../../repositories/users/users";
+import * as bcrypt from "bcrypt";
+
 import { IUser } from "../../interfaces/interfaces";
 
 export const createUser = async (userData: IUser) => {
   try {
+    const salt = await bcrypt.genSalt();
+    userData.password = await bcrypt.hash(userData.password, salt);
     const response = await repositories.createUser(userData);
     return response;
   } catch (error) {
