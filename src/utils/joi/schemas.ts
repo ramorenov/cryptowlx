94b687ceeb242e6809c2057b2da327d474839d19
ricardo <ricardo.moreno@osgroup.co>
2021-07-24 19:45:21 -0500

    FIX valid pref_currencies

--- src/utils/joi/schemas.ts
+++ src/utils/joi/schemas.ts
@@ -10,11 +10,11 @@ export const user = joi.object({
     .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
     .required()
     .label("La contraseña debe ser alfanumerica y tener minimo, 8 caracteres"),
-  pref_currency: joi.string().valid("usd", "euro", "ars").required().label("Ingrese el tipo de moneda preferido entre usd, euro, ars"),
+  pref_currency: joi.string().valid("usd", "eur", "ars").required().label("Ingrese el tipo de moneda preferido entre usd, euro, ars"),
 });
 
 export const coinList = joi.object({
-  vs_currency: joi.string().valid("usd", "euro", "ars").required().label("Ingrese el tipo de moneda preferido entre usd, euro, ars"),
+  vs_currency: joi.string().valid("usd", "eur", "ars").required().label("Ingrese el tipo de moneda preferido entre usd, euro, ars"),
   ids: joi.string().label("Ingrese el symbolo de la criptomoneda"),
   order: joi.string().valid("market_cap_desc", "market_cap_asc").label("Ingrese un nombre de usuario válido"),
   per_page: joi.number().label("Ingrese un valor numerico para paginación"),
