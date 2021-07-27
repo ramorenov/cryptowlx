f109f70fa86f60fc0bc38508f50f45040e1571c0
ricardo <ricardo.moreno@osgroup.co>
2021-07-27 11:34:26 -0500

    DELETE unused module

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,5 @@
 import { Application } from "express";
 import * as controllerUser from "../controllers/users/users";
-import { verifyToken } from "../middlewares";
 
 export function users(app: Application) {
   app.post("/users", controllerUser.createUser);
